e9cf919cf77db1d5bd9807bee78999fb320ed5eb
v-sivozhelezov <@sivozhelezov.v@gmail.com@>
2023-08-01 23:14:51 +0300

    Добавлен свой personalKey

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
+const personalKey = "sivozhelezov";
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
