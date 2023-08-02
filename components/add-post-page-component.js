d7fce842a87ba52ab8224e20f9c87e8e05539653
v-sivozhelezov <@sivozhelezov.v@gmail.com@>
2023-08-02 22:18:28 +0300

    Отрисована страница добавления поста

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -2,12 +2,31 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
-    <div class="page-container">
-      <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
-    </div>
-  `;
+      <div class="page-container">
+          <div class="header-container"></div>
+          <div class="form">
+              <h3 class="form-title">
+               Добавить пост
+                </h3>
+              <div class="form-inputs">
+    
+            
+                      <div class="upload-image-container">
+                        <label class="file-upload-label secondary-button">
+                          <input type="file" class="file-upload-input" style="display:none">Выберите фото
+                        </label>
+                      </div>
+                      <label>
+                      Опишите фотографию:
+                      <textarea class="input textarea" rows="4"></textarea>
+                      </label>                  
+                  
+                  <button class="button" id="login-button">Опубликовать</button>
+              </div>
+            
+          </div>
+      </div>    
+`;
 
     appEl.innerHTML = appHtml;
 
